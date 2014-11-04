fb3a5d9bdc3cd893db4e25d3b443ccafbee77e57
Obed Ampah <Oampah@gmail.com>
2014-11-04 12:28:04 -0500

    2014-11-03_Admin-Dashboard-and-Widgets

--- inc/custom_widgets.php
+++ inc/custom_widgets.php
@@ -39,10 +39,6 @@ $custom_dashboard_widgets = array(
     'me-quickactions-menu' => array(
         'title' => 'Quick Actions',
         'callback' => 'socrates_quickactions_menu'
-    ),
-    'me-dashboard-schedule' => array(
-        'title' => 'Show Schedule',
-        'callback' => 'socrates_dashboard_schedule'
     )
 );
 
@@ -50,13 +46,6 @@ function socrates_quickactions_menu()
 {
     $user = wp_get_current_user();
     echo '<p class="intro">Hello <strong>' . $user->user_firstname . '</strong>.  What would you like to do?</p>';
-}
-
-function socrates_dashboard_schedule()
-{
-    echo '<iframe onload="iFrameHeight()" id="blockrandom" name="iframe" src="http://pw.myersinfosys.com/mhz/hour" width="100%" height="1650" scrolling="auto" align="top" frameborder="0" class="wrapper">
-This option will not work correctly. Unfortunately, your browser does not support inline frames.</iframe>';
-
 }
 
 ?>
